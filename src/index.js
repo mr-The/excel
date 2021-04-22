eea36d4c48c933057a923cc3107552006e1f2912
Oleg Nazarov <rostovshanin@gmail.com>
2021-04-22 22:28:39 +0300

    Finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-import './module'
 import './scss/index.scss'
+import {Excel} from '@/components/excel/Excel';
+import {Header} from '@/components/header/Header';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Formula} from '@/components/formula/Formula';
+import {Table} from '@/components/table/Table';
+
+// eslint-disable-next-line no-unused-vars
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
